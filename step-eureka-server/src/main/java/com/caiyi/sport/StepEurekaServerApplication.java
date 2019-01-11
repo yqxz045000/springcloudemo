36de1ab0c94cffa596b7c58b5b937210f4ca8820
B-0257 <B-0257@B-0257-PC>
2019-01-11 15:25:58 +0800

    集群配置

--- step-eureka-server/src/main/java/com/caiyi/sport/StepEurekaServerApplication.java
+++ step-eureka-server/src/main/java/com/caiyi/sport/StepEurekaServerApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 
+ * @author B-0257
+ *
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class StepEurekaServerApplication {
